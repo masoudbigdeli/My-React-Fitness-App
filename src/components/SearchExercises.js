8e29cd3b2a7e98b7bf7bf8a341dca16d1d76ce39
masoudbigdeli <masoud8bigdeli@gmail.com>
2022-08-23 05:07:24 +0430

    until time 45:50

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Stack, TextField, Typography} from '@mui/material'
+
+const SearchExercises = () => {
+    return (
+        <Stack alignItems='center' mt='37px' justifyContent='center' p="20px">
+        <Typography textAlign='center' fontWeight='700' sx={{fontSize:{lg:'44px',xs:'30px'}}} mb='50px'>
+            Awsome Exercises You <br/> Should Know
+        </Typography>
+        <Box position='relative' mb='72px'>
+            <TextField
+            sx={{
+                input:{
+                    fontWeight:'700',
+                    border:'none',
+                    borderRadius:'4px'},
+                width:{lg:'1170px', xs:'350px'},
+                backgroundColor:'#fff',
+                borderRadius: '40px'
+            }}
+                height='76px'
+                value=''
+                onChange={(e) => {}}
+                placeholder='Search Exercise'
+                type="text"
+            />
+            <Button className="search-btn"
+             sx={{
+                 bgcolor:'#FF2625',
+                 color:"#fff",
+                 textTransform:'none',
+                 width:{lg:"175px", xs:"80"},
+                 fontSize:{lg:'20px', xs:'14px'},
+                 height:"56px",
+                 position:"absolute",
+                 right:"0"
+             }}>
+                Search
+            </Button>
+        </Box>
+
+        </Stack>
+    )
+}
+
+export default SearchExercises;
